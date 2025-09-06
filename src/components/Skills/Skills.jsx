18309fb92f23514980cb5e4bc47f783d1d2fb607
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-09-06 12:44:05 +0530

    about section updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div></div>
+    <section className='py-4 px-[7vw] md:px-[7vw] lg:md-[20vw] font-sans mt-16 md:mt-24 lg:mt-32' id = 'skills'>
+      <div>
+        <h2 className=''>SKILLS</h2>
+        <div className='border-[#8245ec] w-2 h-1'></div>
+      </div>
+      <div></div>
+    </section>
   )
 }
 
