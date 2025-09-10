6c968c235e172fc6ce3c639167aacbcf776bc5b7
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-09-10 22:14:05 +0530

    new

--- constants.js
+++ constants.js
@@ -1,76 +1,90 @@
-import bootstrap from './src/assets/Techlogo/bootstrap.png'
-import c from '././src/assets/Techlogo/c.png'
-import cpp from '././src/assets/Techlogo/cpp.png'
-import css from '././src/assets/Techlogo/css.png'
-import express from '././src/assets/Techlogo/express.png'
-import figma from '././src/assets/Techlogo/figma.png'
-import firebase from '././src/assets/Techlogo/firebase.png'
-import git from '././src/assets/Techlogo/git.png'
-import github from '././src/assets/Techlogo/github.png'
-import html from '././src/assets/Techlogo/html.png'
-import java from '././src/assets/Techlogo/java.png'
-import javascript from '././src/assets/Techlogo/javascript.png'
-import mc from '././src/assets/Techlogo/mc.png'
-import mongodb from '././src/assets/Techlogo/mongodb.png'
-import mysql from '././src/assets/Techlogo/mysql.png'
-import netlify from '././src/assets/Techlogo/netlify.png'
-import nodejs from '././src/assets/Techlogo/nodejs.png'
-import python from '././src/assets/Techlogo/python.png'
-import reactjs from '././src/assets/Techlogo/reactjs.png'
-import tailwindcss from '././src/assets/Techlogo/tailwindcss.png'
-import vercel from '././src/assets/Techlogo/vercel.png'
-import vscode from '././src/assets/Techlogo/vscode.png'
-import materialUI from '././src/assets/Techlogo/materialui.png'
-import typeScript from '././src/assets/Techlogo/typescript.png'
-import postman from '././src/assets/Techlogo/postman.png'
-import compass from '././src/assets/Techlogo/mc.png'
+import bootstrap from "./src/assets/Techlogo/bootstrap.png";
+import c from "././src/assets/Techlogo/c.png";
+import cpp from "././src/assets/Techlogo/cpp.png";
+import css from "././src/assets/Techlogo/css.png";
+import express from "././src/assets/Techlogo/express.png";
+import figma from "././src/assets/Techlogo/figma.png";
+import firebase from "././src/assets/Techlogo/firebase.png";
+import git from "././src/assets/Techlogo/git.png";
+import github from "././src/assets/Techlogo/github.png";
+import html from "././src/assets/Techlogo/html.png";
+import java from "././src/assets/Techlogo/java.png";
+import javascript from "././src/assets/Techlogo/javascript.png";
+import mc from "././src/assets/Techlogo/mc.png";
+import mongodb from "././src/assets/Techlogo/mongodb.png";
+import mysql from "././src/assets/Techlogo/mysql.png";
+import netlify from "././src/assets/Techlogo/netlify.png";
+import nodejs from "././src/assets/Techlogo/nodejs.png";
+import python from "././src/assets/Techlogo/python.png";
+import reactjs from "././src/assets/Techlogo/reactjs.png";
+import tailwindcss from "././src/assets/Techlogo/tailwindcss.png";
+import vercel from "././src/assets/Techlogo/vercel.png";
+import vscode from "././src/assets/Techlogo/vscode.png";
+import materialUI from "././src/assets/Techlogo/materialui.png";
+import typeScript from "././src/assets/Techlogo/typescript.png";
+import postman from "././src/assets/Techlogo/postman.png";
+import compass from "././src/assets/Techlogo/mc.png";
 
+export const skillInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: html },
+      { name: "CSS", logo: css },
+      { name: "JavaScript", logo: javascript },
+      { name: "React", logo: reactjs },
+      { name: "Bootstrap", logo: bootstrap },
+      { name: "Tailwind", logo: tailwindcss },
+      { name: "Material UI", logo: materialUI },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node JS", logo: nodejs },
+      { name: "Express JS", logo: express },
+      { name: "MY SQL", logo: mysql },
+      { name: "Mongo DB", logo: mongodb },
+      { name: "Firebase", logo: firebase },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: c },
+      { name: "C++", logo: cpp },
+      { name: "JavaScript", logo: javascript },
+      { name: "Python", logo: python },
+      { name: "Java", logo: java },
+      { name: "TypeScript", logo: typeScript },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: github },
+      { name: "GitHub", logo: github },
+      { name: "VS Code", logo: vscode },
+      { name: "Postman", logo: postman },
+      { name: "Vercel", logo: vercel },
+      { name: "Compass", logo: compass },
+      { name: "Vercel", logo: vercel },
+      { name: "Netlify", logo: netlify },
+      { name: "Figma", logo: figma },
+    ],
+  },
+];
 
-
-export const skillInfo = [{
-  title: 'Frontend',
-  skills: [
-    {name:'HTML', logo:html},
-    {name:'CSS', logo:css},
-    {name:'JavaScript', logo: javascript},
-    {name:'React', logo: reactjs},
-    {name:'Bootstrap', logo: bootstrap},
-    {name:'Tailwind', logo: tailwindcss},
-    {name:'Material UI', logo: materialUI}
-  ]
-}, 
-{title:'Backend',
-  skills:[
-    {name:'Node JS', logo:nodejs},
-    {name:'Express JS', logo:express},
-    {name:'MY SQL', logo:mysql},
-    {name:'Mongo DB', logo:mongodb},
-    {name:'Firebase', logo:firebase},
-  ]
-},
-{
-  title:'Languages',
-  skills:[
-    {name:'C', logo:c},
-    {name:'C++', logo:cpp},
-    {name:'JavaScript', logo:javascript},
-    {name:'Python', logo:python},
-    {name:'Java', logo:java},
-    {name:'TypeScript', logo:typeScript},
-  ]
-},
-{
-  title:'Tools',
-  skills:[
-    {name:'Git', logo:github},
-    {name:'GitHub', logo:github},
-    {name:'VS Code', logo:vscode},
-    {name:'Postman', logo:postman},
-    {name:'Vercel', logo:vercel},
-    {name:'Compass', logo:compass},
-    {name:'Vercel', logo:vercel},
-    {name:'Netlify', logo:netlify},
-    {name:'Figma', logo:figma}
-  ]
-}
-]
+export const Projects = [
+  {
+    id: 0,
+    title: "WanderStay",
+    description:
+      "WanderStay is a modern platform inspired by Airbnb that connects hosts with travelers looking for short-term stays, homestays, and local experiences. Whether you're a property owner looking to rent out space or a traveler in search of affordable and unique accommodations, WanderStay makes it simple, secure, and convenient.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Node JS", "Express", "MongoDB", "Netlify"],
+    github:
+      "https://github.com/abhijeetxrao/WanderStay",
+    webapp: "https://68beb44e7ac4f7b8294f03d7--cheery-flan-80aca1.netlify.app/",
+  },
+];
